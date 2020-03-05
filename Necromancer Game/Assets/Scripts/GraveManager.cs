ae5b2da289d2ea9feb264f9ba7ba8bc57fec021e
Jack Fletcher <Jackfletch4@gmail.com>
2020-03-05 20:58:20 +0000

    Reworked grave script into Gravestone, added CharacterCreator and CharacterStats script

---
+++ Necromancer Game/Assets/Scripts/GraveManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class GraveManager : MonoBehaviour
+
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    private Grave m_grave;
+    /// <summary>
+    /// 
+    /// </summary>
+    private GameObject[] m_graveSpots;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private void Awake()
+    {
+        m_graveSpots = GameObject.FindGameObjectsWithTag("Grave");
+    }
+    /// Start is called before the first frame update
+    void Start()
+    {
+        Setup();       
+       
+    }
+
+    private void Setup()
+    {
+        
+        foreach (GameObject go in m_graveSpots)
+        {
+            Grave _grave = go.GetComponentInChildren<Grave>();
+            Gravestone _gravestone = go.GetComponentInChildren<Gravestone>();
+            int _idx = UnityEngine.Random.Range(0, Enum.GetValues(typeof(Part_Type)).Length);
+            Part_Type _pt = (Part_Type)_idx;
+            int idx = UnityEngine.Random.Range(0, Enum.GetValues(typeof(Class_Type)).Length);
+            Class_Type _ct = (Class_Type)idx;
+            ///Set text on grave
+            _gravestone.SetCanvasText(_ct.ToString());
+
+            ///Add body part to grave
+            _grave.SetBodyPart(_pt, _ct);
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
---
+++ Necromancer Game/Assets/Scripts/KnightStats.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnightStats : CharacterStats
+{
+    ///// <summary>
+    ///// 
+    ///// </summary>
+    //private void Awake()
+    //{
+       
+    //    CalculateStats();
+        
+    //}
+
+    //public override void CalculateStats()
+    //{
+    //    base.CalculateStats();
+    //    ///Decide attack type
+    //    m_attackType = Attack_Type.Physical;
+    //    ///Calculate attributes
+    //    m_endurance = Random.Range(7, 10);
+    //    m_dexterity = Random.Range(4, 10);
+    //    m_strength = Random.Range(5, 10);
+    //    m_intelligence = Random.Range(2, 10);
+
+    //    ///Calculate Stats from attributes
+
+    //    m_maxHealth = m_endurance;
+    //    m_currentHealth = m_maxHealth;
+    //    m_movementSpeed = m_dexterity / 2;
+    //    m_attackSpeed = m_dexterity / 2;
+    //    m_physicalDamage = m_strength / 2;
+    //    m_spellDamage = m_intelligence / 2;
+    //    m_physicalResist = m_endurance / 10;
+    //    m_spellResist = m_intelligence / 10;
+    //}
+
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+      
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+}
